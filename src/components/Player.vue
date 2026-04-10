ca1422b85eaed9a0398a87722af946d99ecc5391
Grégori Poloni Machado <104400655+gregoripoloni@users.noreply.github.com>
2026-04-09 22:52:25 -0300

    refactor: update Player component layout and styling for improved visual feedback and responsiveness

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -13,13 +13,11 @@
 
 <template>
   <div class="flex flex-col relative items-center justify-between p-2 rounded-xl border-2 border-surface-800 transition-colors lg:flex-row lg:gap-2">
-    <Transition>
-      <div
-        v-if="isActive"
-        class="absolute rounded-full bottom-full w-7 h-[2px] lg:bottom-auto lg:right-full lg:w-[2px] lg:h-7"
-        :class="isCurrentPlayer ? 'bg-sky-400' : 'bg-primary-400'"
-      />
-    </Transition>
+    <div
+      v-if="isActive"
+      class="absolute -left-[2px] -top-[2px] -right-[2px] -bottom-[2px] rounded-xl border-2"
+      :class="{ 'border-primary-400!': isActive && !isCurrentPlayer, 'border-sky-400!': isActive && isCurrentPlayer, 'animate-pulse': isActive }"
+    />
     <div class="flex flex-col items-center gap-2 w-full overflow-x-hidden lg:flex-row">
       <div
         class="flex items-center justify-center w-8 h-8 shrink-0 rounded-full bg-surface-400"
