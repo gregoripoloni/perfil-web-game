c4cbe12f0a474184052f7c94641c3f05393aeade
Grégori Poloni Machado <104400655+gregoripoloni@users.noreply.github.com>
2026-04-21 22:15:51 -0300

    feat: update layout and styling of StartView component for improved UI

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -25,10 +25,18 @@
   <div class="h-full p-2">
     <Card class="Start h-full col-span-3 overflow-y-auto bg-surface-950! border-2 border-surface-800">
       <template #content>
-        <div class="flex flex-col h-full p-2 gap-2 justify-center">
-          <h1 class="text-6xl font-bold text-center mb-10 text-shadow-sm text-shadow-primary-400">Perfil Web Game</h1>
-          <SelectCard text="Nova sala" @click="handleJoinRoom(generateRoomId())" />
-          <SelectCard text="Entrar em uma sala" @click="isRoomDialogVisible = true;" />
+        <div class="flex flex-col h-full p-2 gap-2 justify-between">
+          <div class="flex items-center justify-center h-full">
+            <h1 class="text-6xl font-bold text-center text-shadow-sm text-shadow-primary-400">
+              Perfil
+              <br />
+              <span class="text-4xl">Versão Web</span>
+            </h1>
+          </div>
+          <div class="flex flex-col gap-2">
+            <SelectCard text="Nova sala" @click="handleJoinRoom(generateRoomId())" />
+            <SelectCard text="Entrar em uma sala" @click="isRoomDialogVisible = true;" />
+          </div>
           <RoomDialog v-model:visible="isRoomDialogVisible" @join-room="handleJoinRoom" />
         </div>
       </template>
